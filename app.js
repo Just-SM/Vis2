4e097e85b08fb0895e01f9c574381b6cd618f501
Roman <romafurzykov@gmail.com>
2025-01-05 15:20:26 +0100

    main update

--- app.js
+++ app.js
@@ -1,95 +1,62 @@
-import {Deck,MapController,COORDINATE_SYSTEM} from '@deck.gl/core';
-import {GeoJsonLayer,TextLayer,BitmapLayer, ArcLayer} from '@deck.gl/layers';
-import {CSVLoader} from '@loaders.gl/csv';
+/*
+* https://deck.gl/docs/api-reference/aggregation-layers/heatmap-layer
+*/
+
+import {Deck,} from '@deck.gl/core';
+import { HeatmapLayer } from '@deck.gl/aggregation-layers';
 import {CollisionFilterExtension} from '@deck.gl/extensions'
+import {TextLayer,ScatterplotLayer} from '@deck.gl/layers';
+import {CSVLoader} from '@loaders.gl/csv';
+import {H3ClusterLayer} from '@deck.gl/geo-layers';
 
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-// const COUNTRIES =
-//   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
-// const AIR_PORTS =
-//   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+const heatmapLayer = new HeatmapLayer({
+  id: 'heatmap-layer',
+  data: [
+    { position: [3.5, 8], weight: 1 },
+    { position: [3.8, 7.7], weight: 2 },
+  ],
+  getPosition: d => d.position,
+  getWeight: d => d.weight,
+  pickable: true, // Enable picking for interaction
+});
 
-const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
-  zoom: 4,
-  bearing: 0,
-  pitch: 30
-};
-
-let opacity = 0.1
-
-// class MyMapController extends MapController {
-
-//   handleEvent(event) {
-//     if (event.type === 'wheel') {
-
-//       if (event.delta < 0){
-//         opacity /= 1.55
-//       }
-//       else{
-//         opacity *= 1.55
-//       }
-//       if (opacity <= 0.1) {opacity = 0.1}
-//       if (opacity >= 1) {opacity = 1}
-//       console.log(opacity)
-//       deck.setProps({
-//         layers: [
-//           new GeoJsonLayer({
-//             id: 'base-map',
-//             data: COUNTRIES,
-//             // Styles
-//             stroked: true,
-//             filled: true,
-//             lineWidthMinPixels: 2,
-//             opacity: 0.4,
-//             getLineColor: [60, 60, 60],
-//             getFillColor: [200, 200, 200]
-//           }),
-//           new GeoJsonLayer({
-//             id: 'airports',
-//             data: AIR_PORTS,
-//             extensions: [new CollisionFilterExtension()],
-//             // Styles
-//             filled: true,
-//             opacity: opacity,
-//             pointRadiusMinPixels: 2,
-//             pointRadiusScale: 2000,
-//             getPointRadius: f => 11 - f.properties.scalerank,
-//             getFillColor: [200, 0, 80, 180],
-//             // Interactive props
-//             pickable: true,
-//             autoHighlight: true,
-//             onClick: info =>
-//               // eslint-disable-next-line
-//               info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-//           }),
-//         ]
-
-//       })
-
-
-
-//       super.handleEvent(event);
-//     } else {
-//       super.handleEvent(event);
-//     }
-//   }
-// }
-
-const deck = new Deck({
-  initialViewState: INITIAL_VIEW_STATE,
-  // controller: {type: MyMapController},
-  controller: {inertia: true,scrollZoom: {smooth : 1}},
-  layers: [
-
-    new TextLayer({
-      id: 'ScatterplotLayer',
-      data: 'data.csv',
+    const hex3layer = new H3ClusterLayer({
+      id: 'H3ClusterLayer',
+      data: 'hex_data.json',
       
-      loaders: [CSVLoader],
+      stroked: true,
+      getHexagons: d => d.hexIds,
+      getFillColor: d => [d.color[0], d.color[1], d.color[2]],
+      getLineColor: [255, 255, 255],
+      lineWidthMinPixels: 2,
+      pickable: true,
+      opacity: 0.3,
+  
+    });
+
+    const layer = new ScatterplotLayer({
+      id: 'ScatterplotLayer',
+      // data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-stations.json',
+      
+      stroked: true,
+      getPosition: d => d.coordinates,
+      getRadius: d => Math.sqrt(d.exits),
+      getFillColor: [255, 140, 0],
+      getLineColor: [0, 0, 0],
+      getLineWidth: 10,
+      radiusScale: 6,
+      pickable: true
+    });
+
+const textlayer = new TextLayer({
+      id: 'text',
+      parameters: {depthTest: false},
+      data: 'words_emb.json',
+      
+      // loaders: [CSVLoader],
       extensions: [new CollisionFilterExtension()],
-      getCollisionPriority: d => d.weight,
+      getCollisionPriority: d => d.tf_icf *10000 ^ 2,
+      // collisionTestProps: {radiusScale: 100},
 
       loadOptions: {
         csv: {
@@ -98,54 +65,28 @@ const deck = new Deck({
         },
       },
       
-      getPosition: d => [d.x * 10, (d.y*10) - 20],
-      getText: d => d.word,
-      getAlignmentBaseline: 'center',
-      getColor: [255, 128, 0],
-      getSize:  d => d.weight * 10,
-      getTextAnchor: 'middle',
+      getPosition: d => [d.coord[0],d.coord[1]],
+      getText: d => d.words,
+      // getAlignmentBaseline: 'center',
+      getColor: [70,70, 70],
+      getSize:  d => (d.tf_icf * 3000) +10 ,
+      // getTextAnchor: 'middle',
       pickable: true
-    }),
+    })
 
-    // new GeoJsonLayer({
-    //   id: 'base-map',
-    //   data: COUNTRIES,
-    //   // Styles
-    //   stroked: true,
-    //   filled: true,
-    //   lineWidthMinPixels: 2,
-    //   opacity: 0.4,
-    //   getLineColor: [60, 60, 60],
-    //   getFillColor: [200, 200, 200]
-    // }),
-    // new GeoJsonLayer({
-    //   id: 'airports',
-    //   data: AIR_PORTS,
-    //   // Styles
-    //   extensions : [new CollisionFilterExtension()],
-    //   filled: true,
-    //   opacity: 0.5,
-    //   pointRadiusMinPixels: 10,
-    //   pointRadiusScale: 2000,
-    //   getPointRadius: f => 11 - f.properties.scalerank,
-    //   getFillColor: [200, 0, 80, 180],
-    //   // Interactive props
-    //   pickable: true,
-    //   autoHighlight: true,
-    //   onClick: info =>
-    //     // eslint-disable-next-line
-    //     info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-    // }),
-    // new ArcLayer({
-    //   id: 'arcs',
-    //   data: AIR_PORTS,
-    //   dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-    //   // Styles
-    //   getSourcePosition: f => [-0.4531566, 51.4709959], // London
-    //   getTargetPosition: f => f.geometry.coordinates,
-    //   getSourceColor: [0, 128, 200],
-    //   getTargetColor: [200, 0, 80],
-    //   getWidth: 1
-    // })
-  ]
+new Deck({
+  // mapStyle: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+
+  initialViewState: {
+    longitude: 3.5,
+    latitude: 8,
+    zoom: 6.5,
+    maxZoom: 20,
+    pitch: 30,
+    bearing: 0
+  },
+  controller: true,
+  
+  layers: [, hex3layer,layer,textlayer,heatmapLayer]
 });
+  
